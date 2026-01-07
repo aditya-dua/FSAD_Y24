245669cf6b952b671e840e6da0a90f2a0a84b199
aditya-dua <adityaduatechm@gmail.com>
2026-01-07 10:51:49 +0530

    Initial commit

---
+++ spring_intro/src/main/java/spring_intro/Greeting.java
@@ -0,0 +1,18 @@
+package spring_intro;
+
+public class Greeting {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void display() {
+		System.out.println("Message :: "+message);
+	}
+}
